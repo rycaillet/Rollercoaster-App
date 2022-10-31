afc9f40d303cb560b750fcfbcbcf142b6e6a189b
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-10-31 14:57:41 -0400

    fixed index.js

--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
 const moongoose = require('mongoose')
 
-const Rollercoaster = require ('./rollercoaster')
+const Rollercoaster = require('./rollercoaster')
 const Guests = require('./guests')
 
-const Rollercoaster = moongoose.model('Rollercoaster', Rollercoaster)
+const Rollercoaster = moongoose.model('Rollercoaster', rollercoasterSchema)
 const Guests = moongoose.model('Guests', guestSchema)
 
 module.exports = {
-    Rollercoaster,
-    Guests
+  Rollercoaster,
+  Guests
 }
