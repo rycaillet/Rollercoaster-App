2f51e6db815acc77d2519e92a075a081edf3f9a9
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-10-31 14:31:22 -0400

    fixed db-index.js

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 let MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/donutDatabase'
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/rollercoasterDatabase'
 
 mongoose
   .connect(MONGODB_URI)
