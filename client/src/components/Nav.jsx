933ec7c6ffb972bdb58496965e7dce373b076092
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-10-31 17:08:03 -0400

    added some more to front end

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+
+  return (
+    <nav className="navbar">
+      <Link></Link>
+    </nav>
+  )
+}
+
+export default Nav
